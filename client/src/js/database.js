faeff022e81b0568ff6a55177bba6a049ea48a3f
Rafael Herrera <rafaelherrera731@gmail.com>
2022-07-13 12:44:12 -0700

    work on src folder js files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,41 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+//export const putDb = async (content) => console.error('putDb not implemented');
+
+console.log('PUT to the database');
+//creates connection
+  const jateDb = await openDB('jate', 1);
+  //transaction that allows jate and readwrite
+  const tx = jateDb.transaction('jate', 'readwrite');
+  //objectstore
+  const store = tx.objectStore('jate');
+  //put update
+  const request = store.put({ id: 1, value: content });
+  //result
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+//export const getDb = async () => console.error('getDb not implemented');
+
+export const getDb = async () => {
+  console.log('GET all from the database');
+  //creates connection
+  const jateDb = await openDB('jate', 1);
+  //transaction that allows jate and readonly
+  const tx = jateDb.transaction('jate', 'readonly');
+  //objectstore
+  const store = tx.objectStore('jate');
+  //getall update
+  const request = store.getAll();
+  //result
+  const result = await request;
+  console.log('result.value', result);
+  return result[0].value;
+};
+
 
 initdb();
